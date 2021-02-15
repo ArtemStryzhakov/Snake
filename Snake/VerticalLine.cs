e79d7648a3b5bcc42db19d875b5c279b7e36f326
Opilane TTHK <opilane@tthk.ee>
2021-02-15 14:29:12 +0200

    Add project files.

---
+++ Snake/VerticalLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> vList;
+        public VerticalLine(int xLeft, int xRight, int y, char sym)
+        {
+
+        }
+    }
+}
