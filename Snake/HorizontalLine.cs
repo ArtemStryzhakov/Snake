80059d76d5ce7b658d758cd0ed7ebeed921a1f7a
opilane <opilane@HP-CZC6297B8C>
2021-02-19 11:20:57 +0200

    Upgrade

--- Snake/HorizontalLine.cs
+++ Snake/HorizontalLine.cs
@@ -8,13 +8,13 @@ namespace Snake
     {
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             pList = new List<Point>();
             for (int x = xLeft; x <= xRight; x++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
-            
         }
     }
 }
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -2,28 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Snake
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
 
-            //Отрисовка рамочки
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '-');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '-');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, 'I');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, 'I');
-            upLine.Drow();
-            downLine.Drow();
-            leftLine.Drow();
-            rightLine.Drow();
+			Console.SetWindowSize(80, 25);
 
-            //Отрислвка точек
-            Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-        }
-    }
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
+
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
+
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
+
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
+
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+			WriteGameOver();
+			Console.ReadLine();
+		}
+
+
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("-----------------------------", xOffset, yOffset++);
+			WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("Autor: Artem Stryzhakov", xOffset + 2, yOffset++);
+			WriteText("Special for Marina Oleinik.", xOffset + 1, yOffset++);
+			WriteText("-----------------------------", xOffset, yOffset++);
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+
+	}
 }
